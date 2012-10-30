76d1534d87d86631b75da5d9ebd3eeb60988da5e
antonydoyle <antony@siege21.com>
2012-10-30 15:39:46 +0000

    Removed some custom styling that broke some functions

--- html/mod_login/default.php
+++ html/mod_login/default.php
@@ -52,21 +52,15 @@ JHtml::_('behavior.keepalive');
     
     <p class="toggleLabel"><span>Hide password</span></p>
 
-    <div class="toggle-switch">
-    <input type="checkbox" id="toggle-1" checked="" onchange="document.getElementById('modlgn-passwd').type = this.checked ? 'password' : 'text'"/>
-    <div class="bg"></div>
-    <label for="toggle-1"><span>ON</span><span>OFF</span></label>
-    </div>
+    <input type="checkbox" id="toggle-1" checked="checked" onchange="document.getElementById('modlgn-passwd').type = this.checked ? 'password' : 'text'"/>
+    
    
        </div>
        <div class="six columns padded">
 		<p class="toggleLabel"><span><?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?></span></p>
         
-             <div class="toggle-switch">
             <input id="toggle-2" checked="checked" type="checkbox" name="remember" class="inputbox"/>
-            <div class="bg"></div>
-            <label for="toggle-2"><span>YES</span><span>NO</span></label>
-            </div>
+            
         </div>
 	</p>
 	<?php endif; ?>
